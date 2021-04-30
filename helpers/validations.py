1b620c18fcbda7e2dd595946f9e88de8d04bfd95
Fadi Hussein <developer.fadi.m.hussein@gmail.com>
2021-04-30 07:00:01 +0300

    Fixed Link in License Header

--- helpers/validations.py
+++ helpers/validations.py
@@ -1,5 +1,5 @@
 #  Copyright (C) 2021 Fadi Hussein
-#  This file is part of blender-sculpting-layers <https://github.com/FadiMHussein/blender-sculpting-layers.git>.
+#  This file is part of blender-sculpting-layers <https://github.com/FadiMHussein/blender-sculpting-layers>.
 #
 #  blender-sculpting-layers is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
